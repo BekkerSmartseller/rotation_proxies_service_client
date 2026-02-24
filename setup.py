b9c5c8b8f3f4c7a6ce5222f0ab8c2bad086a8a4e
BekkerSmartseller <bekker.smartseller@gmail.com>
2026-02-24 18:54:59 +0200

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wb_api_client",
-    version="0.1.9",
+    version="0.1.10",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
